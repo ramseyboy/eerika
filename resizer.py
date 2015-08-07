3940e01bfc127c043d67e80f2c8727eec8783c78
ramseyboy <walker.hannan@willowtreeapps.com>
2015-08-06 20:30:02 -0400

    fixed multifile resizing, added ignore file

--- resizer.py
+++ resizer.py
@@ -3,10 +3,12 @@ import os
 import PIL
 from PIL import Image
 
-class resizer():
+
+class Resizer():
+
     def __init__(self, dirname):
         self._dirname = dirname
-        self._img_counter = {'total': 0, 'failed': 0}
+        self._img_counter = {'total': 0, 'success': 0, 'failed': 0}
 
     def process_images(self):
         if os.path.isdir(self._dirname):
@@ -16,33 +18,32 @@ class resizer():
         else:
             self.resize_image(self._dirname)
         print("Of", self._img_counter['total'], "image(s):", self._img_counter['failed'],
-        "failed and", self._img_counter['failed'] - self._img_counter['total'],  "passed")
-        # if self._img_counter['failed'] == 0:
-        #     print("Your resized files are now located in", os.path.join(os.path.abspath(os.path.dirname(__file__)), self._dirname + '/resized/'))
-        # for filename in os.listdir(self._dirname):
-        #     open(filename)
+        "failed and", self._img_counter['success'],  "passed")
 
     def resize_image(self, filename):
         try:
-            self._img_counter['total'] = self._img_counter['total'] + 1          
+            self._img_counter['total'] = self._img_counter['total'] + 1   
+
             img = Image.open(filename)
             basewidth = 300
             wpercent = (basewidth / float(img.size[0]))
             hsize = int((float(img.size[1]) * float(wpercent)))
             img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
             if not os.path.exists('photos/resized'):  
-                os.mkdir('photos/resized')
+                os.mkdir('photos/resized')  
 
-            resizedFilename = 'photos/resized/{0}_resized.jpg'.format(1)        
+            resizedFilename = '{0}'.format(os.path.basename(filename))
 
             if os.path.exists(resizedFilename):
                 os.remove(resizedFilename)
 
-            img.save(resizedFilename)
+            img.save(os.path.join(self._dirname, "resized/", resizedFilename))
+
+            self._img_counter['success'] = self._img_counter['success'] + 1
         except IOError as e:
             self._img_counter['failed'] = self._img_counter['failed'] + 1           
             print(e)
 
 if __name__ == '__main__':      
-    r = resizer("photos")
+    r = Resizer("photos")
     r.process_images()
